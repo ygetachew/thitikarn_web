a318484a78289aa9fef302e27bbdb1fb0458935d
Yared Getachew Tessema <yared@gemhalo.org>
2011-06-20 10:16:39 +0300

    local modifications

--- templates/rhuk_milkyway/index.php
+++ templates/rhuk_milkyway/index.php
@@ -38,7 +38,8 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 			<div id="header">
 				<div id="header_l">
 					<div id="header_r">
-						<div id="logo"></div>
+						<div id="logo"></div>						
+						<div id="logo_text">THITIKARN</div>						
 						<jdoc:include type="modules" name="top" />
 					</div>
 				</div>
